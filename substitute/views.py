0a945f925a4cc77c95f78d90bf8ab17acf6e2dab
Azer1338 <Azergh**1338>
2020-05-06 17:42:43 +0200

    add the checkboxes filter for the nutriscore

--- substitute/views.py
+++ substitute/views.py
@@ -12,9 +12,32 @@ def search_view(request):
     :param request: User's request
     :return:
     """
+    # Initiate some variables
+    # Generate a dictionary to collect user'choices
+    nutriscore_wished_list = []
+    # List the checkboxes in the template
+    nutriscore_dict = {'nutriscore_a': 'a',
+                       'nutriscore_b': 'b',
+                       'nutriscore_c': 'c',
+                       'nutriscore_d': 'd',
+                       'nutriscore_e': 'e'
+                       }
+    # List of aliment
+    aliment_list = []
+    # Context
+    check_box_choices = ""
 
     # Collect the User's request
     query = request.GET.get('userSearch')
+
+    # Collect the User's choices
+    for key, values in nutriscore_dict.items():
+        if request.GET.get(key):
+            nutriscore_wished_list.append(values)
+            # Ad user'choices in the context
+            check_box_choices = check_box_choices + key + "=on&"
+
+    print(check_box_choices)
 
     # Ensure that the query is filled
     if query:
@@ -24,45 +47,41 @@ def search_view(request):
         if not aliment_name.exists():
             # No result found message
             message = "Misère de misère, nous n'avons trouvé aucun résultat !"
-            aliment_list = []
         else:
             # Look for similar aliment
             message = "Vous pouvez remplacer cet aliment par:"
             # Collect a list of aliment with the same categories
-            aliment_list = Aliment.objects.\
-                filter(category__icontains=aliment_name[0].category).\
-                order_by('nutrition_score')
+            for elt in nutriscore_wished_list:
+                search = Aliment.objects.filter(category__icontains=aliment_name[0].category,
+                                                nutrition_score__icontains=elt).order_by('nutrition_score')
+                aliment_list.extend(search)
 
     else:
         # No query send
         message = "Vous n'avez pas spécifié votre recherche. Voici notre liste."
-        aliment_list = Aliment.objects.all().order_by('nutrition_score')
+        for elt in nutriscore_wished_list:
+            search = Aliment.objects.filter(nutrition_score__icontains=elt).order_by('nutrition_score')
+            aliment_list.extend(search)
 
     # Slice page
     paginator = Paginator(aliment_list, 6)
+    print(paginator.num_pages)
     # Get the current page
-    page = request.GET.get('page')
+    page_number = request.GET.get('page')
 
-    # Return only this page substitute and not others
-    try:
-        aliments = paginator.page(page)
-        print("Page OK")
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        aliments = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        aliments = paginator.page(paginator.num_pages)
+    pag_obj = paginator.get_page(page_number)
 
     # Build the context
     context = {
-        'substitutes': aliments,
+        'substitutes': pag_obj,
 
         'user_request': query,
+        'check_boxes_status': check_box_choices,
         'message': message,
 
         'paginate': True,
     }
+    print(context)
 
     return render(request, 'substitute/search.html', context)
 
@@ -89,7 +108,7 @@ def detail_view(request, aliment_code):
     }
     # Add user'information to the feed
     if request.user.is_authenticated:
-        context.update({'user_city': current_user.city})
+        context.update({'user_city': current_user.address})
 
     return render(request, 'substitute/details.html', context)
 
